291cd6605bfc0c87feda08efd17fea0737f5ca25
Abhinav <abhiraja1012@gmail.com>
2019-02-02 17:19:40 -0500

    First version of chatApp

---
+++ app.js
@@ -0,0 +1,40 @@
+// This is a file for the server-side
+// Import statments
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs'); // set the template engine ejs
+app.use(express.static('public'));
+app.get('/', (req, res) => { // like routing in Vue
+    res.render('index')
+});
+server = app.listen(3000); // make the 'server' listen to app on port 3000
+
+//configure socket.io
+const io = require('socket.io')(server) // instantiate socket.io. The io object will give us access to the socket.io lib
+io.on('connection', (socket) => {
+    console.log('New user connected'); // server listens, and for on every new connection, console.log it
+    socket.username = "Anonymous"; // when a new user connects, give user a default name of Anon
+    socket.on('change_username', (data) => {
+        socket.username = data.username // when the change username button is pressed, change the that user's/socket's username
+    });
+    socket.on('new_message', (data) => { // when server receives a new message, send it out to all users, with username and message data
+        io.sockets.emit('new_message', { // sockets = all sockets connected
+            message: data.message,
+            username: socket.username
+        });
+    });
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', { // broadcast means to send to all sockets except the socket that started it
+            username: socket.username
+        });
+    });
+
+    socket.on('no_longer_typing', (data) => {
+        socket.broadcast.emit('no_longer_typing', { // broadcast means to send to all sockets except the socket that started it
+            username: socket.username
+        });
+
+        console.log("Server: received not typing");
+    });
+});
---
+++ public/chat.js
@@ -0,0 +1,60 @@
+$(function () { // the $() indicates jQuery function
+    var socket = io.connect('http://localhost:3000') // client makes a connection to the server
+
+    var message = $("#message")
+    var username = $("#username")
+    var send_message = $("#send_message")
+    var send_username = $("#send_username")
+    var chatroom = $("#chatroom")
+    var feedback = $("#feedback")
+    var typing = false;
+    var timeout = undefined;
+    var typing = false;
+    var timeout = undefined;
+
+    send_username.click(function () { // when the user clicks on the change username button, send a message to server through socket
+        console.log(username.val());
+        socket.emit('change_username', {
+            username: username.val()
+        });
+    });
+
+    socket.on("new_message", (data) => { // listen for a new message, and when triggered, display the new message on local user's screen
+        console.log(data);
+        feedback.replaceWith("");
+        chatroom.append("<p class = 'message'>" + data.username + ":  " + data.message + "</p>");
+
+    });
+
+    send_message.click(function () { // when user clicks send, send a message to the server
+        socket.emit('new_message', {
+            message: message.val()
+        });
+        $("#message").val(""); // to clear input text field when button is pressed
+    });
+    message.bind("keypress", () => { // send a message to server that current user is typing
+        //socket.emit('typing');
+        if (typing == false) {
+            typing = true
+            socket.emit('typing');
+            timeout = setTimeout(timeoutFunction, 1000);
+        } else {
+            clearTimeout(timeout);
+            timeout = setTimeout(timeoutFunction, 1000);
+        }
+    });
+    socket.on('typing', (data) => { // get info if another user is typing
+        $("#feedback").html("<p><i>" + data.username + " is typing a message..." + "</i></p>");
+    });
+
+    socket.on('no_longer_typing', (data) => { // get info if another user has stopped typing
+        $("#feedback").html("");
+        console.log("Client: received other's not typing");
+    });
+
+    function timeoutFunction() {
+        typing = false;
+        socket.emit('no_longer_typing');
+        console.log("current user not typing");
+    }
+});
